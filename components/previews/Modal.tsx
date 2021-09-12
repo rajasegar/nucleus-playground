a94cce644fc4c526670e01100ca9d509ae64ca30
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-09-12 11:30:00 +0530

    chore: bump react-nucleus and fix modal preview

--- components/previews/Modal.tsx
+++ components/previews/Modal.tsx
@@ -1,6 +1,30 @@
 import React, { useState } from 'react';
 import { Button, Modal } from '@freshworks/react-nucleus';
+import styled from 'styled-components'
+import {Flex} from '@rebass/grid'
 
+const ModalBody = styled.div`
+  padding: 18px 32px 32px 32px;
+`
+
+const ModalFooter = styled.div`
+  ${({
+    theme: {
+      colors: {
+        components: {confirmModal},
+      },
+    },
+  }) => {
+    return `
+    justify-content: flex-end;
+    display: flex;
+    padding: 12px 16px 12px 16px;
+    border-top: 1px solid ${confirmModal.border};
+    background: ${confirmModal.background};
+    border-radius: 4px;
+  `
+  }}
+`
 const MockContent = ({close}: {close: () => void}) => {
   return (
     <>
