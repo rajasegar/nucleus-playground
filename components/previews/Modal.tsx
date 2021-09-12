87108f01e21b123e582a787128d71639a0406022
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-09-12 11:15:13 +0530

    chore: add mockcontent component to modal preview

--- components/previews/Modal.tsx
+++ components/previews/Modal.tsx
@@ -1,6 +1,31 @@
 import React, { useState } from 'react';
 import { Button, Modal } from '@freshworks/react-nucleus';
 
+const MockContent = ({close}: {close: () => void}) => {
+  return (
+    <>
+      <ModalBody>
+        <h4>Title goes here</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta,
+          dolore excepturi velit ut, ea earum ipsum aspernatur modi ex ipsa
+          repudiandae eaque amet sequi eligendi deserunt libero nobis asperiores
+          necessitatibus!
+        </p>
+      </ModalBody>
+      <ModalFooter>
+        <Flex>
+          <Button type="secondary" onClick={close}>
+            Cancel
+          </Button>
+          <div style={{paddingLeft: '15px'}}>
+            <Button onClick={close}>Accept</Button>
+          </div>
+        </Flex>
+      </ModalFooter>
+    </>
+  )
+}
 export default function() {
      const [showDialog, setShowDialog] = useState(false)
   const open = () => setShowDialog(true)
@@ -11,9 +36,8 @@ export default function() {
                 Open Dialog
             </Button>
             <Modal isOpen={showDialog} onDismiss={close}>
-              {/* <MockContent close={close} /> */}
-                <h1>Hello World</h1>
-            </Modal>
+                <MockContent close={close} />
+          </Modal>
         </div>
     )
 }
